77fd4509f00dfd081b1d477165c1593029c47eaa
Nouraiz <tnouraiz@hlbb.hongleong.com.my>
2023-08-03 22:42:02 +0800

    Initial Commit

---
+++ NBKAssessment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NBKAssessment
+//
+//  Created by Nouraiz Taimour on 03/08/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
